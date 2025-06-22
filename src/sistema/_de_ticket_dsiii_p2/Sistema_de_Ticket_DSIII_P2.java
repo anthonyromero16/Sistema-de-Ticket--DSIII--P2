73c2cacc61f265014df5edb4d56d5a54c9b7a717
antho <antho@Anthony>
2025-06-22 00:33:45 -0500

    Inicializacion de proyecto

---
+++ src/sistema/_de_ticket_dsiii_p2/Sistema_de_Ticket_DSIII_P2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema._de_ticket_dsiii_p2;
+
+/**
+ *
+ * @author antho
+ */
+public class Sistema_de_Ticket_DSIII_P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
